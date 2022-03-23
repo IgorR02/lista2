431542499e1a3f762536092b0d3a4a655a41e46a
IgorR02 <igor.ropiak@gmail.com>
2022-03-23 10:37:59 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,71 @@
+import math
+import sys
+
+#Zad1
+# list = ["Snowboard", "Strzelectwo", "Bilard", "Kalistnika"]
+# list.reverse()
+# list.append("Bieganie")
+# print(list)
+
+#Zad2
+# dictionary = {"FAQ" : "Frequently Asked Questions","FYI" : "For Your Information","NP" : "Na przyklad","TZN" : "To Znaczy" }
+# print(dictionary)
+
+#Zad3
+# dictionary2 = {"Fallout" : "76", "Call Of Duty" : "MW"}
+# print(len(dictionary2))
+
+#Zad4
+# a = input("Podaj zdanie: ")
+# print(a.count("a"))
+
+#Zad5
+# print("Podaj 3 liczby calkowite: ")
+# data = sys.stdin.readline()
+# file = open("abc.txt", "w+")
+# file.write(data)
+# result = a**b + c
+# print(str(result))
+
+#Zad6
+# a = int(input("Podaj liiczbe calkowita nr 1:"))
+# b = int(input("Podaj liiczbe calkowita nr 2:"))
+# c = int(input("Podaj liiczbe calkowita nr 3:"))
+# if a > b:
+#     if a > c:
+#         print("Liczba nr 1 jest najwieksza")
+# elif b > a:
+#     if b > c:
+#         print("Liczba nr 2 jest najwieksza")
+# elif c > a:
+#     if c > b:
+#         print("Liczba nr 3 jest najwieksza")
+# else:
+#     print("Liczby sa takie same")
+
+#Zad7
+# list = [2, 2.5, 3, 3.5]
+# l = len(list)
+# for x in range(0, l):
+#     list[x] = list[x] ** 2
+# print(list)
+
+#Zad8
+# a = 0
+# list = []
+# while a != 10:
+#     b = int(input("Podaj liczbe calkowita:"))
+#     a+=1
+#     if b % 2 == 0:
+#         list.append(b)
+# print(list)
+
+#Zad9
+# root = int(input("Podaj liczbe do spierwiastkowania: "))
+# if root < 0:
+#     print("Podano bledna liczbe!")
+# else:
+#     print(math.sqrt(root))
+
+
+
